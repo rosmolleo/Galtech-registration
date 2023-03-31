66c4136083949369ea4eca8d9398ec9abe4f30b3
rosmolleo <122086803+rosmolleo@users.noreply.github.com>
2023-03-31 11:54:02 +0530

    Update Mysql_connect.php

--- Mysql_connect.php
+++ Mysql_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","admin","admin","galtech_membership_details");
+$conn=mysqli_connect("localhost","root","","galtech_membership_details");
 //database creation
 // $db_create="CREATE  DATABASE galtech_membership_details";
 // $conn->query($db_create);
